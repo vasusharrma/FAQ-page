80b878a60baec2ec9d7af63d6ab9b42b5e66868e
vasusharrma <sharmavashu099@gmail.com>
2024-08-11 00:03:35 +0530

    update js

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const allBtn = document.querySelectorAll(".desc_btn");
 
 buttonContainer.addEventListener("click", (element) => {
   let btnclick = element.target.classList.contains("desc_btn");
-  element.target.classList.toggle("rotate");
-console.log(element);
 
   if (btnclick) {
+
+    allBtn.forEach((e)=>{
+    let descPara = e.closest('.items').children[1];
+        console.log(descPara);
+        if(descPara.classList.contains('active') && (e != element.target)){
+            descPara.classList.remove('active');
+        }
+    })
+
+    element.target.classList.toggle("rotate");
     const parentElement = element.target.closest(".items");
     parentElement.children[1].classList.toggle("active");
   }
